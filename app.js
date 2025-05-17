639328d8c17f0b05cf4f37b299eba4a29d329414
jimmysaqipi <saqipi.mergim@gmail.com>
2025-05-17 11:38:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/testdb', {
+  useNewUrlparser: true,
+  useUnifiedTopology: true,
+})
+.then (() =>console.log('U lidh me MongoDB!'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res)=>{
+  res.send('Hello from Dockerfile');
 });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
 });
